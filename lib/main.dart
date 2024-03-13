ab3c876418a84d7f54cfdff21f8538903dea64aa
GuiKrause <guilhermekrauseramos@gmail.com>
2024-03-13 09:16:20 -0400

    Added new file, with a statefullWidget which deal the logic of randomizer a number and update the state of the value

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:roll_dice/gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepOrangeAccent,
         body: GradientContainer(
-          Color.fromARGB(255, 255, 153, 0),
-          Color.fromARGB(255, 255, 86, 35),
+          const Color.fromARGB(255, 255, 153, 0),
+          const Color.fromARGB(255, 255, 86, 35),
         ),
       ),
     ),
